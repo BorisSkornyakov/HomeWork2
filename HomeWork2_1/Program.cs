9724fa866b03d8186a49eb715b8570c6adcdaae7
BorisSkornyakov <3601124@gmail.com>
2022-09-05 10:00:51 +0500

    Домашнее задание ко второму семинару

---
+++ HomeWork2_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int Transform(int a)
+{
+    return (a/10) % 10;
+}
+System.Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(System.Console.ReadLine());
+System.Console.WriteLine($"{number} -> {Transform(number)}");
---
+++ HomeWork2_3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.WriteLine("Введите номер дня недели от 1 до 7, где 1 это Понедельник, а 7 это Воскресенье");
+int numberday = Convert.ToInt32(Console.ReadLine());
+if ((numberday == 6) || (numberday == 7))
+{
+    System.Console.WriteLine($"{numberday} -> да");
+}
+else if (numberday > 7)
+{
+    System.Console.WriteLine($"Нет такого дня недели");
+}
+else
+{
+    System.Console.WriteLine($"{numberday} -> нет");
+}
